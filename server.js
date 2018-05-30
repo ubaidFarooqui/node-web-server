915ec0fe638fa39b0053cef6b12aa846fc6e13df
Ubaid Ahmed Farooqui <farooqui.ubaid@gmail.com>
2018-05-30 11:48:39 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,195 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs')
+
+
+app.use((req, res, next) => { // this is express middle ware that we created
+    
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append server.log');
+        }
+    });
+    next();
+});
+
+//app.use((req, res, next) => {
+//    
+//    res.render('maintainance.hbs');
+//    
+//});
+
+app.use(express.static(__dirname + '/public'));
+ 
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    
+    return text.toUpperCase();
+    
+});
+
+app.get('/', (req, res) => {
+    
+   res.render('home.hbs', {
+       
+      pageTitle: 'Home Page',
+      welcomeMessage: 'Welcome to my Website',
+       
+   });
+});
+
+app.get('/about', (req, res) => {
+    
+    res.render('about.hbs', {
+        
+        pageTitle: 'About Us',
+        para: 'Screaming try on'
+    });
+    
+});
+
+app.get('/bad', (req, res) => {
+    
+    res.send({
+        
+       errorMessage: 'Unable to handle request'
+        
+    });
+    
+});
+
+app.listen(3000, () => {
+  
+    console.log('Server is up on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function getRecipe() {
+//    
+//   setTimeout(() => {
+//       
+//      const recipeId = [1,2,3,4];
+//      console.log(recipeId);
+//       
+//      setTimeout ((id) => {
+//          
+//          const recipe1 = {title: 'Pizza', publisher: 'smith' };
+//          console.log(`${id} and ${recipe1.title}`);
+//                      
+//                      setTimeout((publisher) => {
+//              
+//              const recipe2 = {title: 'Italian Pizza', publisher: 'John'};
+//              console.log(recipe1);
+//          }, 1500, recipe1.publisher);
+//          
+//      },1500, recipeId[3]);
+//       
+//   },1500);
+//    
+//}
+//
+//console.log('app start');
+//getRecipe();
+//
+//const getId = new Promise((resolve, reject) => {
+//    
+//    setTimeout( () => {
+//        
+//        resolve([435, 987, 541, 908]);
+//        
+//    }, 1500);
+//});
+//
+//getId.then((result) => {
+//    
+//    console.log(result);
+//})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
